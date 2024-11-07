fa8c6c3c1a268b257880982c02d7ddbe0d13c60f
Hellen830 <liamedwards9544@gmail.com>
2024-11-07 00:00:00 -0800

    Fake commit 34

---
+++ r3live/src/meshing/vcg/wrap/system/multithreading/scoped_read_lock.h
@@ -0,0 +1,36 @@
+#ifndef MT_SCOPED_READ_LOCK_H
+#define MT_SCOPED_READ_LOCK_H
+
+#include "rw_lock.h"
+
+namespace mt
+{
+
+class scoped_read_lock
+{
+	MT_PREVENT_COPY(scoped_read_lock)
+
+	public:
+
+		typedef scoped_read_lock this_type;
+		typedef void             base_type;
+
+		scoped_read_lock(rw_lock & rwl) : rw(rwl)
+		{
+			this->rw.lock_read();
+		}
+
+		~scoped_read_lock(void)
+		{
+			this->rw.unlock_read();
+		}
+
+	protected:
+
+		rw_lock & rw;
+};
+
+}
+
+#endif // MT_SCOPED_READ_LOCK_H
+Fake commit 34 on 2024-11-07T00:00:00
